3f201faf4fa32ab8d358c7a957032248e4b4d7b1
SzymonSG <sg.szymon8@gmail.com>
2022-03-14 11:44:49 +0100

    test token

--- src/main/java/com/cinema/booking/security/model/LoginModel.java
+++ src/main/java/com/cinema/booking/security/model/LoginModel.java
@@ -6,14 +6,15 @@ import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
 
 @Getter
+
 @Setter
 @NoArgsConstructor
 public class LoginModel {
 
     private String email;
-
     private String password;
 
 
