6013c51b80e2c7a041fd759dca50c3c5ce7d88a4
SzymonSG <sg.szymon8@gmail.com>
2022-03-17 22:30:17 +0100

    Rename aspect

---
+++ src/main/java/com/cinema/booking/aop/ValidMovieAspect.java
@@ -0,0 +1,25 @@
+package com.cinema.booking.aop;
+
+import com.cinema.booking.entities.Movie;
+import com.cinema.booking.exceptions.ConstraintViolationException;
+import com.cinema.booking.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class ValidMovieAspect {
+
+    private final MovieRepository movieRepository;
+
+    @Before("execution(* com.cinema.booking.services.movieService.MovieServiceImpl.movieSave(..)) && args(movie))")
+    public void checkSeatingAreUnique(Movie movie) throws ConstraintViolationException {
+        boolean seatingExist = movieRepository.existsBySeating(movie.getSeating());
+        if (seatingExist){
+            throw new ConstraintViolationException("This seat is already saved");
+        }
+    }
+}
